1951a5cb7644ed46704e0bbf3b94d630c9a3c953
sixvxsh <sixvxsh.j@gmail.com>
2023-08-12 14:17:33 +0330

    added test to reverse

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,7 +6,9 @@ function reversestr(str) {
     // use the reverse method of array
     // convert back the array into string
     // return the result
-
+    const arr =  str.split('');
+    const arrReverse = arr.reverse();
+    return arrReverse.join('');
 
 }
 
