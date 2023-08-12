aada20ca362d4a6b66fd17cdd578c58529161864
sixvxsh <sixvxsh.j@gmail.com>
2023-08-12 14:02:36 +0330

    init

---
+++ reversestring/index.js
@@ -0,0 +1,13 @@
+
+
+function reversestr(str) {
+
+    // convert string into array
+    // use the reverse method of array
+    // convert back the array into string
+    // return the result
+
+
+}
+
+module.exports = reversestr
