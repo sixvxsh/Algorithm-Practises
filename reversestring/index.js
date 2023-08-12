3e09315b3269504d54b1241b467845b122ac73b1
sixvxsh <sixvxsh.j@gmail.com>
2023-08-12 14:45:29 +0330

    Updated reverse

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,10 +6,23 @@ function reversestr(str) {
     // use the reverse method of array
     // convert back the array into string
     // return the result
-    const arr =  str.split('');
-    const arrReverse = arr.reverse();
-    return arrReverse.join('');
+
+    // const arr =  str.split('');
+    // const arrReverse = arr.reverse();
+    // return arrReverse.join('');
+
+
+    let reversed = '';
+
+    for(let char of str) {
+        reversed = char + reversed;
+    }
+    return reversed;
 
 }
-
+reversestr('sia');
 module.exports = reversestr
+
+
+//first solution
+// return str.split('').reverse().join('');
